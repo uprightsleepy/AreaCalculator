f4ef470178bebe06a24d4a5c427ca6810f193bb5
Henry <designsbyhenryp@gmail.com>
2021-03-23 22:20:59 -0700

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,31 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
+
+    }
+
+    public static double area(double radius){
+        double pi = Math.PI;
+        if(radius < 0){
+            return -1.0;
+        }
+        else{
+            return (radius * radius) * pi;
+        }
+    }
+
+    public static double area(double x, double y){
+        if(x < 0 || y < 0){
+            return -1.0;
+        }
+        else{
+            return x * y;
+        }
+    }
+}
